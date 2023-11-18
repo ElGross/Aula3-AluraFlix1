11697214e829858daa01f6497eddd7e73cc56e70
ElGross <151382540+ElGross@users.noreply.github.com>
2023-11-18 13:15:38 -0300

    Lista nomes filmes
    Lista de nomes de filmes com link para os respectivos banner's.

---
+++ script.js
@@ -0,0 +1,12 @@
+document.write('<a href="https://img.travessa.com.br/DVD/BA/58/58a198c9-8478-49a7-9675-cacfce3b5e78.jpg"> <font color="white">1 - Re-Animator: A Hora dos Mortos-Vivos</font> </a>' + '<br />'); //Re-Animator: A Hora dos Mortos-Vivos
+ 
+
+document.write('<a href="https://br.web.img3.acsta.net/pictures/15/04/29/19/23/173745.jpg"> <font color="white">2 - O Ataque dos Tomates Assassinos</font> </a>' + '<br />'); //O Ataque dos Tomates Assassinos
+
+document.write('<a href="https://images.justwatch.com/poster/244597010/s718/rubber-pneu.jpg"> <font color="white">3 - Rubber, O Pneu Assassino</font> </a>' + '<br />'); //Rubber, O Pneu Assassino
+
+document.write('<a href="https://media.fstatic.com/MY2P09UYdjoFO9640Tj9DetF3nc=/322x478/smart/filters:format(webp)/media/movies/covers/2011/05/9f48449e5ca313d0cda4494b223ab4af.jpg"> <font color="white">4 - Palhaços Assassinos do Espaço Sideral</font> </a>' + '<br />'); //Palhaços Assassinos do Espaço Sideral
+
+document.write('<a href="https://br.web.img2.acsta.net/c_310_420/pictures/15/05/04/22/05/380484.jpg"> <font color="white">5 - Sharknado</font> </a>' + '<br />'); //Sharknado
+
+document.write('<a href="https://br.web.img3.acsta.net/pictures/210/150/21015012_20130624195436012.jpg"> <font color="white">6 - Planeta Terror</font> </a>' + '<br />'); //Planeta Terror
